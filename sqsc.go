d50b12d419bdbf9d439327255f4417a64eeaf62f
chase <chase.isabelle@vestiairecollective.com>
2021-04-12 22:01:08 +0200

    ok

--- sqsc.go
+++ sqsc.go
@@ -2,19 +2,20 @@ package sqsc
 
 import (
 	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-// the client
+// SQSC the client
 type SQSC struct {
 	sqs    *sqs.SQS
 	config Config
 }
 
-// the client configs
+// Config the client configs
 type Config struct {
 	ID       string //<< aws account id
 	Key      string //<< aws auth key - leave blank for no auth
@@ -28,7 +29,7 @@ type Config struct {
 	Wait     int    //<< wait time (seconds)
 }
 
-// creates a new client instance
+// New creates a new client instance
 func New(cfg *Config) (*SQSC, error) {
 	// default is no-auth
 	crd := credentials.AnonymousCredentials
@@ -77,14 +78,14 @@ func New(cfg *Config) (*SQSC, error) {
 	}, err
 }
 
-// produce a new message on the queue
+// Produce produce a new message on the queue
 //
-// bod - the message body
-// del - the delay in seconds (usually just use 0)
+// 	* bod - the message body
+// 	* del - the delay in seconds (usually just use 0)
 //
 // returns
-// - the message id
-// - error
+// 	* the message id
+// 	* error
 func (c *SQSC) Produce(bod string, del int) (string, error) {
 	// send message
 	inp := sqs.SendMessageInput{
@@ -115,57 +116,117 @@ func (c *SQSC) Produce(bod string, del int) (string, error) {
 	return id, err
 }
 
-// consume a single message from the queue
+// Consume consume a single message from the queue
 //
 // returns
-// - the message body
-// - the receipt handle (use for deleting messages)
-// - any error
+// 	* the message body
+// 	* the receipt handle (use for deleting messages)
+// 	* any error
 func (c *SQSC) Consume() (string, string, error) {
-	// receive message
-	res, err := c.sqs.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl:          aws.String(c.config.URL),
-		VisibilityTimeout: aws.Int64(int64(c.config.Timeout)),
-		WaitTimeSeconds:   aws.Int64(int64(c.config.Wait)),
-	})
+	// receive messages
+	bods, rhs, err := c.Receive(1)
 
-	// default message body
+	// prep the results
 	bod := ""
 	rh := ""
 
-	// get message body if we can
-	if res != nil && len(res.Messages) != 0 {
+	// check error
+	if err != nil {
+		return bod, rh, err
+	}
+
+	// quick validation
+	lb := len(bods)
+	lrh := len(rhs)
+
+	if lb != lrh {
+		err = fmt.Errorf("body count and receipt handle mismatch: %d != %d", lb, lrh)
+	}
+
+	// check if we have any results
+	if lb > 0 {
+		bod = bods[0]
+	}
+
+	if lrh > 0 {
+		rh = rhs[0]
+	}
+
+	// return the results
+	return bod, rh, err
+}
+
+// Receive consume multiple messages from the queue
+//
+// 	* n - the number of messages to consume (max 10)
+//
+// returns
+// 	* the message bodies
+// 	* the receipt handles (use for deleting messages)
+// 	* any error
+func (c *SQSC) Receive(n int64) ([]string, []string, error) {
+	// receive message
+	res, err := c.sqs.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(c.config.URL),
+		VisibilityTimeout:   aws.Int64(int64(c.config.Timeout)),
+		WaitTimeSeconds:     aws.Int64(int64(c.config.Wait)),
+		MaxNumberOfMessages: aws.Int64(n),
+	})
+
+	// check the response
+	if res == nil && err == nil {
+		err = errors.New("received nil response with no error")
+	}
+
+	// prep the results
+	var bods []string
+	var rhs []string
+
+	// did it work?
+	if err != nil {
+		return bods, rhs, err
+	}
+
+	// build the results
+	for _, msg := range res.Messages {
 		// get the body pointer first
-		msg := res.Messages[0]
 		ptr := msg.Body
 
 		// make sure we can dereference it
-		if ptr != nil {
-			// dereference it
-			bod = *ptr
+		if ptr == nil {
+			err = errors.New("received nil message body")
+
+			break
 		}
+
+		// dereference and append it
+		bods = append(bods, *ptr)
 
 		// get the receipt handle pointer
 		ptr = msg.ReceiptHandle
 
 		// make sure we can dereference it
-		if ptr != nil {
-			// dereference it
-			rh = *ptr
+		if ptr == nil {
+			err = errors.New("received nil receipt handle")
+
+			break
 		}
+
+		// dereference and append it
+		rhs = append(rhs, *ptr)
 	}
 
 	// we done fam
-	return bod, rh, err
+	return bods, rhs, err
 }
 
-// delete a message from the queue
+// Delete delete a message from the queue
 //
-// rh - the receipt handle (from sqsc.Consume())
+// 	* rh - the receipt handle (from sqsc.Consume())
 //
 // returns
-// - the response (will be empty if success)
-// - any error
+// 	* the response (will be empty if success)
+// 	* any error
 func (c *SQSC) Delete(rh string) (string, error) {
 	// delete that pesky message
 	res, err := c.sqs.DeleteMessage(&sqs.DeleteMessageInput{
